1c282e440c44d944243ca5f67e11b191c8049d45
Josh Cheek <josh.cheek@gmail.com>
2014-09-29 07:46:20 -0600

    Name more magic vars, fixed display bug

--- FrontEnd.elm
+++ FrontEnd.elm
@@ -9,7 +9,8 @@ import Char
 -- Named Values
 standardsBG    = rgb 60 100 60
 standardWidth  = 200
-standardHeight = 200
+standardHeight = 50
+standardOffset = 10
 
 -- views
 standardView message =
@@ -19,14 +20,16 @@ standardView message =
 
 display : Standards.State -> Element
 display state =
-  let positionFor n               = move (0, 60*n)
+  let totalStandardHeight         = standardHeight+standardOffset
+      positionFor n               = move (0, totalStandardHeight*n)
       positionStandards index svs = if | isEmpty svs -> []
                                        | otherwise   -> (positionFor index (head svs)) :: (positionStandards (index+1) (tail svs))
       standardViews               = map standardView state.standards
-  in  collage 500 500 <| [move (0, -60 * 3) (group <| positionStandards 0 standardViews)]
+  in  collage 500 500 <| [move (0, -totalStandardHeight * 3) (group <| positionStandards 0 standardViews)]
 
 defaultStandardsState =
   let defaultStandards = ["omg1", "omg2", "omg3", "omg4"]
-  in  lift (Standards.State defaultStandards 0) Keyboard.lastPressed
+      currentIndex     = 0
+  in  lift (Standards.State defaultStandards currentIndex) Keyboard.lastPressed
 
 main = lift display defaultStandardsState
