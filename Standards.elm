40a28c132707d1bd3d1659289da583e5c891c800
Josh Cheek <josh.cheek@gmail.com>
2014-09-29 06:28:36 -0600

    Aesthetics

--- Standards.elm
+++ Standards.elm
@@ -9,19 +9,19 @@ import Char
 -- toForm (if state == Play then spacer 1 1 else identity msg)
 display : StandardsState -> Element
 display standardsState =
-  let pongGreen                  = rgb 60 100 60
-      displayStandard message    = group [ filled pongGreen (rect 200 50)
-                                         , toForm message
-                                         ]
-      positionFor n              = move (0, 60*n)
-      displayStandards ss n      = if | isEmpty ss -> []
-                                      | otherwise  -> (positionFor n (displayStandard <| head ss)) :: (displayStandards (tail ss) (n+1))
-      strings                    = map plainText (.standards standardsState)
+  let pongGreen               = rgb 60 100 60
+      displayStandard message = group [ filled pongGreen (rect 200 50)
+                                      , toForm message
+                                      ]
+      positionFor n           = move (0, 60*n-60*3)
+      displayStandards ss n   = if | isEmpty ss -> []
+                                   | otherwise  -> (positionFor n <| displayStandard <| head ss) :: (displayStandards (tail ss) (n+1))
+      strings                 = map plainText (.standards standardsState)
   in  collage 500 500 (displayStandards strings 0)
 
-type StandardsState = { standards:[String]
-                      , currentIndex:Int
-                      , lastPressed:Int
+type StandardsState = { standards    : [String]
+                      , currentIndex : Int
+                      , lastPressed  : Int
                       }
 
 defaultStandardsState = let defaultStandards = [ "omg1"
