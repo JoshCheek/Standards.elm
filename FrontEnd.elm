b0397adc764957f10f8ab15340d6f787ba99f0d8
Josh Cheek <josh.cheek@gmail.com>
2014-09-29 07:34:12 -0600

    WIP: broke some stuff, but I just thought of something.... the cache!

--- FrontEnd.elm
+++ FrontEnd.elm
@@ -6,19 +6,24 @@ import Keyboard
 import Text
 import Char
 
--- colours
-standardsBG = rgb 60 100 60
+-- Named Values
+standardsBG    = rgb 60 100 60
+standardWidth  = 200
+standardHeight = 200
 
 -- views
+standardView message =
+  group [ filled standardsBG (rect standardWidth standardHeight)
+        , toForm message
+        ]
+
 display : Standards.State -> Element
-display standardsState =
-  let displayStandard message = group [ filled standardsBG (rect 200 50)
-                                      , toForm message
-                                      ]
-      positionFor n           = move (0, 60*n)
+display state =
+  let positionFor n           = move (0, 60*n)
+      standardViews           = map standardView state.standards
       displayStandards ss n   = if | isEmpty ss -> []
-                                   | otherwise  -> (positionFor n <| displayStandard <| head ss) :: (displayStandards (tail ss) (n+1))
-      strings                 = map plainText (.standards standardsState)
+                                   | otherwise  -> (positionFor n <| head ss) :: (displayStandards (tail ss) (n+1))
+      strings                 = map plainText (.standards state)
   in  collage 500 500 <| [move (0, -60 * 3) (group <| displayStandards strings 0)]
 
 defaultStandardsState =
