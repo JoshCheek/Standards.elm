672bad8929b78db50a757d652725a992c0c0454a
Josh Cheek <josh.cheek@gmail.com>
2014-09-29 10:58:51 -0600

    Fixed the type bug and got it highlighting current

--- FrontEnd.elm
+++ FrontEnd.elm
@@ -7,7 +7,8 @@ import Text
 import Char (KeyCode)
 
 -- Named Values
-standardsBG    = rgb 60 100 60
+standardsBG isCurrent = if | isCurrent -> rgb 80 130 80
+                           | otherwise -> rgb 40 60 40
 standardWidth  = 200
 standardHeight = 50
 standardOffset = 10
@@ -15,7 +16,7 @@ standardOffset = 10
 -- views
 standardView : Bool -> Standard -> Form
 standardView isCurrent standard =
-  group [ filled standardsBG (rect standardWidth standardHeight)
+  group [ filled (standardsBG isCurrent) (rect standardWidth standardHeight)
         , toForm (plainText standard.standard)
         ]
 
@@ -25,10 +26,10 @@ display state =
       positionFor n               = move (0, totalStandardHeight*n)
       positionStandards index svs = if | isEmpty svs -> []
                                        | otherwise   -> (positionFor index (head svs)) :: (positionStandards (index+1) (tail svs))
-      standardViews               = indexedMap (\i s -> standardView ((toFloat i) == (toFloat state.currentIndex)) s) state.standards
+      standardViews               = indexedMap (\i s -> standardView (i==state.currentIndex) s) state.standards
       standardGroup               = group <| positionStandards 0 standardViews
       currentAtCenter             = -totalStandardHeight * state.currentIndex
-  in  collage 500 500 [move (0, currentAtCenter) standardGroup]
+  in  collage 500 500 [move (0, (toFloat currentAtCenter)) standardGroup]
 
 -- Run
 handleEvent keyCode state =
