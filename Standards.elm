c3009f8a989ebe9c73813bc6a77b934223959bde
Josh Cheek <josh.cheek@gmail.com>
2014-09-29 20:29:48 -0600

    Remove the nextSiblingOf code, going to try traversing with a zipper

--- Standards.elm
+++ Standards.elm
@@ -7,33 +7,12 @@ type Standard = { id        : Int
                 }
 
 -- Hierarchy
-data Hierarchy = RootNode
-               | HierarchyNode { id             : Int
-                               , parent_id      : Int
-                               , name           : String
-                               , tags           : [String]
-                               , subhierarchies : [Hierarchy]
-                               }
-
-nextSiblingOf : Hierarchy -> Hierarchy -> Int
-nextSiblingOf root target =
-  1
-  -- findPost root target [root]
-
-findPost : Hierarchy -> Hierarchy -> [Hierarchy] -> Hierarchy
-findPost node target queue =
-  if   isEmpty queue
-  then node
-  else case target of
-       (RootNode) -> node
-       otherwise  ->
-         case node
-         (RootNode) -> findPost (head queue) target (tail queue)
-         otherwise  ->
-           if node.id == target.id
-           then head queue
-           else findPost (head queue) target ((tail queue) ++ node.subhierarchies)
-
+data Hierarchy = Hierarchy { id             : Int
+                           , parentId       : Int
+                           , name           : String
+                           , tags           : [String]
+                           , subhierarchies : [Hierarchy]
+                           }
 
 -- State
 type State = { currentIndex  : Int
@@ -46,4 +25,5 @@ defaultStandardsState =
   let standard1     = Standard 1 "The first standard"  ["tag1", "tag2"]
       standard2     = Standard 2 "The second standard" ["tag1", "tag3"]
       standard3     = Standard 3 "The third standard"  ["tag2", "tag3"]
-  in State 0 RootNode [standard1, standard2, standard3]
+      root          = Hierarchy {id = -1, parentId = -1, name = "root", tags = [], subhierarchies = []}
+  in State 0 root [standard1, standard2, standard3]
