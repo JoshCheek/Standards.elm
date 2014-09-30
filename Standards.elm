f2f4f36ff2d0b19ce8bbc88946c2d1ebacba0401
Josh Cheek <josh.cheek@gmail.com>
2014-09-29 22:08:07 -0600

    Create a tree to "zip" around

--- Standards.elm
+++ Standards.elm
@@ -1,10 +1,12 @@
 module Standards where
 
+
 -- Standard
 type Standard = { id        : Int
                 , standard  : String
                 , tags      : [String]
                 }
+
 
 -- Hierarchy               id  parentId name   tags     subhierarchies
 data Hierarchy = Hierarchy Int Int      String [String] [Hierarchy]
@@ -23,10 +25,32 @@ type State = { currentIndex  : Int
              , standards     : [Standard]
              }
 
+-- 2----4---5
+-- |    |   |
+-- 1-3  6   7-8-9
+--              |
+--              a-b-c
+
 -- add some hierarchies in here
 defaultStandardsState =
   let standard1     = Standard 1 "The first standard"  ["tag1", "tag2"]
       standard2     = Standard 2 "The second standard" ["tag1", "tag3"]
       standard3     = Standard 3 "The third standard"  ["tag2", "tag3"]
-      root          = Hierarchy -1 -1 "root" [] []
+      root          = Hierarchy 0 -1 "root" [] [h2, h4, h5]
+
+      h2            = Hierarchy 2  0 "h2"   [] [h1, h3]
+      h4            = Hierarchy 4  0 "h4"   [] [h6]
+      h5            = Hierarchy 5  0 "h5"   [] [h7, h8, h9]
+
+      h1            = Hierarchy 1  2 "h1"   [] []
+      h3            = Hierarchy 3  2 "h3"   [] []
+      h6            = Hierarchy 6  4 "h6"   [] []
+      h7            = Hierarchy 7  5 "h7"   [] []
+      h8            = Hierarchy 8  5 "h8"   [] []
+      h9            = Hierarchy 9  5 "h9"   [] [ha, hb, hc]
+
+      ha            = Hierarchy 10 9 "ha"   [] []
+      hb            = Hierarchy 11 9 "hb"   [] []
+      hc            = Hierarchy 12 9 "hc"   [] []
+
   in State 0 root [standard1, standard2, standard3]
